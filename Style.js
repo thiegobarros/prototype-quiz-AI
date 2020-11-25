e2abd16983d153f4f03b32ac5f96dc9b8fee1a1f
Thiego Barros <thiegobarros@gmail.com>
2020-11-25 13:32:24 -0400

    style

---
+++ Style.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+import styled from 'styled-components/native';
+
+export const CustomButton = styled.TouchableOpacity`
+    width: 100px;
+    height: 30px;
+    border-radius: 3px;
+    margin-top: 10px
+    background-color: #2097f3;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const CustomButtonText = styled.Text`
+    color: white;
+`;
